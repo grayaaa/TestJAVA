62abfe12ae76f18209f8a17a623d058fd3d82f8c
grayaaa <qmgeng@126.com>
2014-12-17 14:59:33 +0800

    bobo正则test

--- src/TestReg.java
+++ src/TestReg.java
@@ -4,11 +4,18 @@ import java.util.regex.Pattern;
 public class TestReg {
 
     public static void main(String[] args) {
-        String regFilter = "yanyiuser(((?!0{4})\\d{4})|([1-4]\\d{4})|(50000))@163.com";
-        String regValue = "yanyiuser0000@163.com";
+        //String regFilter = "yanyiuser(((?!0{4})\\d{4})|([1-4]\\d{4})|(50000))@163.com";
+        //String regValue = "yanyiuser0000@163.com";
+        String regFilter = "(?!.*bulletin|.*roomId)^.*newsbobo.*";
+        String regFilter2 = "^http://www.bobo.com/special/sp/newsbobo.html(?:.(?!(roomId|bulletin)))*$";
+        String regFilter3 = "(?!.*bulletin|.*roomId)^http://www.bobo.com/special/sp/newsbobo.html.*";
+
+        String regValue1 = "http://www.bobo.com/special/sp/newsbobo.html?viewid=home&NTES&bulletin&logined";
+        String regValue2 = "http://www.bobo.com/special/sp/newsbobo.html?viewid=home&NTES&&roomId&logined";
+        String regValue3 = "http://www.bobo.com/special/sp/newsbobo.html?viewid=home&NTES&logined";
         Boolean flagFilter = true;
         try {
-            flagFilter = Pattern.matches(regFilter, regValue);
+            flagFilter = Pattern.matches(regFilter2, regValue3);
         } catch (Exception e) {
             System.out.println("test");
         }
